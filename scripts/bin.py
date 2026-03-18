b0858109bca75130029f6348dd098fb81f4890cd
althea-mr6 <althea.amaury@efrei.net>
2026-03-18 12:10:03 +0100

    changement du code et creation de dossiers

--- scripts/bin.py
+++ scripts/bin.py
@@ -1,131 +1,17 @@
 import pygame
-from random import random, choice
-from scripts.utils import load_image
+import os
 
+class Bin:
+    def __init__(self, x, y, bin_type):
+        """
+        bin_type : 'bleue', 'jaune', 'marron', 'verte'
+        """
+        self.type = bin_type
+        # Charger l'image correspondante
+        image_path = os.path.join('assets', 'poubelles', f'{bin_type}.png')
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (80, 120))
+        self.rect = self.image.get_rect(center=(x, y))
 
-# load assets
-log_image = load_image('log.png', (60, 30), scale=1.1)
-
-
-bin_images = [
-    load_image('Poubelle Bleue.png', (90, 40), scale=1.2),
-    load_image('Poubelle Jaune.png', (90, 40), scale=1.2),
-    load_image('rock1.png Maron', (90, 40), scale=1.2),
-    load_image('rock2.png Verte', (90, 40), scale=1.2)
-]
-
-class Platform():
-    """
-    The base class for all platforms.
-    """
-    def __init__(self, game, center_x, bottom, image):
-        self.game = game
-        self.image = image
-        self.rect = self.image.get_frect(centerx=center_x, bottom=bottom)
-        self.color = 'grey'
-        self.force = 0
-        self.collectible = None
-        self.offset = 10
-        self.floating = True
-
-    def remove(self):
-        self.game.platforms.remove(self)
-
-    def touch(self):
-        pass
-
-    def push(self):
-        self.force = 5
-
-    def in_screen(self, scroll):
-        return self.rect.right - scroll < self.game.width and self.rect.left - scroll > 0
-
-    def float(self, scroll):
-        indices = range(
-            max(0, int((self.rect.left - scroll) / self.game.water.spacing)),
-            min(self.game.water.num_points, int((self.rect.right - scroll) / self.game.water.spacing))
-        )
-        y = sum(self.game.water.points[i] for i in indices) / len(indices)
-
-        if self.force > 0:
-            self.rect.bottom = y + self.force
-            self.force -= 0.2
-            if self.force <= 0:
-                self.force = 0
-        else:
-            self.rect.bottom = y
-
-    def update(self):
-        pass
-
-    def render(self, surf, scroll):
-        pos = (self.rect.x - scroll, self.rect.y)
-        if self.image:
-            surf.blit(self.image, pos)
-        else:
-            pygame.draw.rect(surf, self.color, pos)
-
-
-
-class Rock(Platform):
-    """
-    The biggest solid platform.
-    """
-    def __init__(self, game, center_x, bottom):
-        img = choice(rock_images)
-        super().__init__(game, center_x, bottom, img)
-        self.color = 'darkgrey'
-
-
-class Lilypad(Platform):
-    """
-    A platform that sinks into the water when the frog touches it for too long.
-    """
-    def __init__(self, game, center_x, bottom):
-        img = choice(lilypad_images)
-        super().__init__(game, center_x, bottom, img)
-        self.color = 'forestgreen'
-        self.timer = 2000
-        self.sinking = False
-        self.touched = False
-
-    def sink(self):
-        # start sinking into the water
-        self.sinking = True
-        self.touched = False
-        self.floating = False
-
-    def touch(self):
-        self.touched = True
-
-    def update(self):
-        if self.touched:
-            # wait 2 seconds before sinking
-            self.timer -= self.game.dt
-            if self.timer <= 0:
-                self.sink()
-        if self.sinking:
-            # sink into the water
-            if self.rect.top < self.game.height:
-                self.rect.y += 7
-            else:
-                self.remove()
-
-
-class Log(Platform):
-    """
-    A moving platform.
-    """
-    def __init__(self, game, center_x, bottom):
-        super().__init__(game, center_x, bottom, log_image)
-        self.color = 'brown'
-        self.direction = 1 if random() > 0.5 else -1
-        self.position = self.rect.centerx
-        self.speed = (0.5 + random()) * 0.8
-
-    def update(self):
-        # infinitely move the platform
-        self.rect.x += self.speed * self.direction
-        if self.rect.right > self.position + 100 or self.rect.left < self.position - 100:
-            self.direction *= -1
-
+    def render(self, surf):
+        surf.blit(self.image, self.rect)
--- scripts/waste.py
+++ scripts/waste.py
@@ -1,108 +1,45 @@
 import pygame
+import os
+from scripts.utils import move
 
-from scripts.utils import move, load_image
-from scripts.bin import
+class Waste:
+    def __init__(self, start_x, start_y, waste_type):
+        """
+        waste_type : 'banane', 'bouteille', 'canette', 'papier'
+        """
+        self.type = waste_type
+        # Association déchet -> bonne poubelle
+        self.correct_bin = {
+            'banane': 'marron',    # déchet compostable
+            'bouteille': 'jaune',  # plastique
+            'canette': 'jaune',    # métal
+            'papier': 'bleue'      # papier/carton
+        }
 
+        # Charger l'image
+        image_path = os.path.join('assets', 'dechets', f'{waste_type}.png')
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (40, 40))
+        self.rect = self.image.get_rect(center=(start_x, start_y))
 
-scale = 0.15
-
-images = {
-    'idle': load_image('Kris.png', scale=scale),
-    'jumping': load_image('Kris 2.png', scale=scale)
-}
-images_divided = {}
-for state, img in images.items():
-    images_divided[state] = pygame.transform.scale(img, (img.get_width() / 2, img.get_height() / 2))
-
-launch_sound = pygame.mixer.Sound('./assets/launch_jump.mp3')
-
-
-class Frog():
-    """
-    The frog controlled by the player.
-    """
-    def __init__(self, center_x, bottom):
-        self.state = 'idle'
-        self.images = images
-        self.image = self.images[self.state]
-        self.base_size = self.image.get_size() # (60, 50)
-        self.size = self.base_size
-        self.rect = pygame.FRect(center_x - self.size[0] / 2, bottom - self.size[1], *self.size)
-        self.jumping = False
-        self.velocity = 0
-        self.init_pos = self.rect.center
+        self.launched = False
+        self.init_pos = (start_x, start_y)
         self.time = 0
         self.angle = 0
         self.power = 0
-        self.plat_mov = False
-        self.platform = None
 
-        def divide_size(self):
-            rect = self.rect.copy()
-            self.rect.width /= 2
-            self.rect.height /= 2
-            self.rect.bottom = rect.bottom
-            self.rect.centerx = rect.centerx
-            self.images = images_divided
-            self.image = self.images[self.state]
+    def launch(self, power, angle):
+        self.launched = True
+        self.init_pos = self.rect.center
+        self.time = 0
+        self.angle = angle
+        self.power = power
 
-        def collide(self, obj):
-            # check for collision between a bin
-            return self.rect.colliderect(obj.rect)
+    def update(self, dt, gravity):
+        if self.launched:
+            self.time += dt / 100
+            new_x, new_y = move(self.init_pos, self.angle, self.power, self.time, gravity)
+            self.rect.center = (new_x, new_y)
 
-        def launch(self, power, angle):
-            # start a new jump
-            launch_sound.play()
-            self.launch = True
-            self.init_pos = self.rect.center
-            self.time = 0
-            self.angle = angle
-            self.power = power
-            self.plat_mov = False
-            self.set_state('launch')
-
-        def set_state(self, state):
-            # set animation state
-            self.state = state
-            self.image = self.images[self.state]
-
-        def update(self, dt, platforms, g):
-            """
-            Update the frog's position and check for collisions.
-            """
-            if self.jumping:
-                # calculate new coordinates during a jump
-                self.time += dt / 100
-                self.rect.center = move(self.init_pos, self.angle, self.power, self.time, g)
-
-                # check for collisions with platforms
-                for platform in platforms:
-                    if self.collide(platform):
-                        self.rect.bottom = platform.rect.top
-                        self.jumping = False
-                        self.set_state('idle')
-                        platform.touch()
-                        # stick to the platform if's is a log
-                        if type(platform) is Log:
-                            self.plat_mov = True
-                            self.platform = platform
-
-            elif self.plat_mov:
-                # move accordingly to the log
-                self.rect.bottom = self.platform.rect.top
-                self.rect.x += self.platform.speed * self.platform.direction
-
-            else:
-                # stay on the ground
-                self.rect.y += 5
-                for platform in platforms:
-                    if self.collide(platform):
-                        self.rect.bottom = platform.rect.top
-
-        def render(self, surf, scroll):
-            surf.blit(self.image, (self.rect.x - scroll, self.rect.y + 3))
-
-
-
-
-
+    def render(self, surf):
+        surf.blit(self.image, self.rect)
